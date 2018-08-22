dfb4c4d5577a3b2e43d9cbc30df2616120ea569f
Adam Thompson <adam@serialphotog.com>
2018-08-22 15:48:39 -0400

    Made the logging component thread-safe.

--- mTiler/Core/Logger.cs
+++ mTiler/Core/Logger.cs
@@ -10,6 +10,8 @@ namespace mTiler.Core
 {
     class Logger
     {
+        delegate void loggingDelegate(String text);
+
         /// <summary>
         /// Reference to the output window
         /// </summary>
@@ -25,14 +27,34 @@ namespace mTiler.Core
         }
 
         /// <summary>
+        /// Logs a message to the output console
+        /// </summary>
+        /// <param name="header">The header that gets printed before each message</param>
+        /// <param name="msg">The message to log</param>
+        /// <param name="color">The text color to log the message in</param>
+        private void log(String header, String msg, Color color)
+        {
+           outputWindow.Invoke((Action)delegate // Make things thread-safe
+           {
+               try
+               {
+                   outputWindow.Select(outputWindow.TextLength, 0);
+                   outputWindow.SelectionColor = color;
+                   outputWindow.AppendText(header + msg + Environment.NewLine);
+               } catch (Exception e)
+               {
+                    // Ignore. This is the result of canceling in the middle of an operation.
+               }
+           });
+        }
+
+        /// <summary>
         /// Logs a message to the output window
         /// </summary>
         /// <param name="msg">The message to log</param>
         public void log(String msg)
         {
-            outputWindow.Select(outputWindow.TextLength, 0);
-            outputWindow.SelectionColor = Color.Black;
-            outputWindow.AppendText("[INFO]: " + msg + Environment.NewLine);
+            log("[INFO]: ", msg, Color.Black);
         }
 
         /// <summary>
@@ -41,9 +63,7 @@ namespace mTiler.Core
         /// <param name="msg">The error message to log</param>
         public void error(String msg)
         {
-            outputWindow.Select(outputWindow.TextLength, 0);
-            outputWindow.SelectionColor = Color.Red;
-            outputWindow.AppendText("[ERROR]: " + msg + Environment.NewLine);
+            log("[ERROR]: ", msg, Color.Red);
         }
     }
 }
