4e7dfdade49841b2cb7d33a4b1e9f925ee3edb73
Adam Thompson <adam@serialphotog.com>
2018-09-19 11:03:04 -0400

    Adjusted white threshold.
    This decreases errors when dealing with light gray colors.

--- mTiler/Core/Data/MapTile.cs
+++ mTiler/Core/Data/MapTile.cs
@@ -36,7 +36,7 @@ namespace mTiler.Core.Data
         /// <summary>
         /// The threshold to use when checking for "white" pixels
         /// </summary>
-        private static readonly int WhiteThreshold = 50;
+        private static readonly int WhiteThreshold = 30;
 
         /// <summary>
         /// Threshold that is used when determining how alike colors are
