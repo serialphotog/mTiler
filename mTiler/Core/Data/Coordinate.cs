edf946387d76681cae6ac219b79cbc221ff4a56d
Adam Thompson <adam@serialphotog.com>
2018-10-22 10:31:53 -0400

    Added a coordinate data structure

---
+++ mTiler/Core/Data/Coordinate.cs
@@ -0,0 +1,29 @@
+﻿namespace mTiler.Core.Data
+{
+    /// <summary>
+    /// Represents an X, Y coordinate
+    /// </summary>
+    struct Coordinate
+    {
+        /// <summary>
+        /// The x component of the coordinate 
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// The y component of the coordinate
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Initializes a new coordinate
+        /// </summary>
+        /// <param name="x">The x component</param>
+        /// <param name="y">The y component</param>
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
